b5b65a24878723a6862fb979c143a5b1259fb8f8
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2022-10-26 22:58:14 +0200

    basic column structure: data and layout

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,11 +1,22 @@
 import "../css/BoardPage.css";
 
 import { Layout } from "antd";
+import { initialDNDData } from "../data";
+import Column from "../components/Column";
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
-      <Layout.Content className="board-content"></Layout.Content>
+      <Layout.Content className="board-content">
+        {initialDNDData.columnOrder.map((columnId: string) => {
+          const column = initialDNDData.columns[columnId as keyof typeof initialDNDData.columns];
+          const tasks = column.taskIds.map(
+            (taskId: string) => initialDNDData.tasks[taskId as keyof typeof initialDNDData.tasks]
+          );
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </Layout.Content>
     </Layout>
   );
 };
